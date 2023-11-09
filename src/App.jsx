5aedb3205ecb0be33199b706cbfd5ffe678f2688
samarth kashyap <samarthkashyap318@gmail.com>
2023-11-09 22:33:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='mx-auto'>
+        <h1 className='text-2xl text-center'>Under Construction</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
