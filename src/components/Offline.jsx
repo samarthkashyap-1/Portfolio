fd20f02819bf8e441d529a0780755bacb7a5015b
samarth kashyap <samarthkashyap318@gmail.com>
2024-01-13 15:21:14 +0530

    Refactor Offline component to use theme from Context

--- src/components/Offline.jsx
+++ src/components/Offline.jsx
@@ -2,15 +2,27 @@ import Lottie from 'lottie-react'
 import React from 'react'
 import load from '../assets/offline1.json'
 
+import { Context } from './Context'
+
+
 
 const Offline = () => {
+  const { theme } = React.useContext(Context);
+
   return (
     <>
       <div className=" flex w-screen h-screen justify-center flex-col items-center md:gap-10">
-        <h1 className="sm:text-2xl md:text-3xl text-6xl leading-loose font-bold sm:leading-relaxed ">
+        <h1
+          className={` sm:text-2xl md:text-3xl text-6xl leading-loose font-bold sm:leading-relaxed text-center  ${
+            theme ? "invert" : "invert-0"
+          }`}
+        >
           You are offline buddy!!!
         </h1>
-        <Lottie animationData={load} className={`w-1/3 md:w-3/4 `} />
+        <Lottie
+          animationData={load}
+          className={`w-1/3 md:w-3/4 ${theme ? "invert" : "invert-0"} `}
+        />
       </div>
     </>
   );
